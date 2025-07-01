f6eedf2d341821b7541d1f29f243c30acb4c239f
Yulian302 <bohomolyulian3022003@gmail.com>
2025-07-01 19:25:01 -0400

    feat: Integrate Redis and implement task queueing
    Implemented simple task queueing to the Redis instance without it's
    further processing.

--- main.go
+++ main.go
@@ -9,14 +9,58 @@ import (
 
 	"github.com/Yulian302/qugopy/config"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 )
 
+var rdb *redis.Client
+var ctx = context.Background()
 
+type Task struct {
+	ID      string `json:"id"`
+	Type    string `json:"type"`
+	Payload string `json:"payload"`
+}
 
 func healthCheckHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"health": "ok",
 	})
+}
+func TaskEnqueueHandler(c *gin.Context) {
+	var task Task
+
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Invdalid request payload",
+			"details": err.Error(),
+		})
+		return
+	}
+
+	taskJson, err := json.Marshal(task)
+	if err != nil {
+		log.Printf("Failed to marshal task: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Internal server error",
+		})
+		return
+	}
+
+	intCmd := rdb.LPush("task_queue", taskJson)
+	if err := intCmd.Err(); err != nil {
+		log.Printf("Redis LPush failed: %v", err)
+		c.JSON(http.StatusServiceUnavailable, gin.H{
+			"error":   "Task queue unavailable",
+			"details": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status": "Task enqueued!",
+		"id":     task.ID,
+	})
+
 }
 
 func main() {
@@ -28,8 +72,12 @@ func main() {
 
 	r := gin.Default()
 
+	rdb = redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%s:%s", cfg.REDIS.HOST, cfg.REDIS.PORT),
+	})
 
 	r.GET("/test", healthCheckHandler)
+	r.POST("/task", TaskEnqueueHandler)
 
 	if err := r.Run(fmt.Sprintf("%s:%s", cfg.HOST, cfg.PORT)); err != nil {
 		log.Fatal("Failed to start server: %w", err)
